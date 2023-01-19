afbe5f5873b87d3614e5ebaad24245cf613e7fad
Daiene Batagioti <daiene.montuani@hotmail.com>
2023-01-19 13:54:59 -0300

    I fixed the card size and started fixing the arrow properties

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,15 @@ const listaSelecao = document.querySelectorAll('.botao');
 const cardapioOpcoes = document.querySelectorAll('.cardapio-opcoes');
 
 listaSelecao.forEach(botao => {
-    botao.addEventListener('click', () => {
+    botao.addEventListener('click', (e) => {
+        e.preventDefault();
+
         const cartaoAberto = document.querySelector('.aberto');
         cartaoAberto.classList.remove('aberto');
 
         const idCartaoSelecionado = botao.attributes.id.value;
 
-        const idDoCartaoParaAbrir = 'cartao-' + idCartaoSelecionado;
+        const idDoCartaoParaAbrir = 'carousel-' + idCartaoSelecionado;
 
         const cartaoParaAbrir = document.getElementById(idDoCartaoParaAbrir);
         cartaoParaAbrir.classList.add('aberto');
