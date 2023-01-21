f52e884543d4272dd31ce1281b42321b0db8a642
Daiene Batagioti <daiene.montuani@hotmail.com>
2023-01-20 23:12:21 -0300

    Carousel with length problem

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,35 @@ listaSelecaoBotoes.forEach((botao) => {
         lancheSelecionadoNasOpcoes.classList.add("ativo");
     })
 })
+
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", () => {
+    const isLeft = control.classList.contains("arrow-left");
+
+    if (isLeft) {
+        currentItem -= 1;
+      } else {
+        currentItem += 1;
+      }
+  
+      if (currentItem >= maxItems) {
+        currentItem = 0;
+      }
+  
+      if (currentItem < 0) {
+        currentItem = maxItems -1;
+      }
+
+    items.forEach((item) => {
+        items[currentItem].scrollIntoView({
+            behavior: "smooth",
+            inline: "center"
+          });
+    });
+  });
+});
