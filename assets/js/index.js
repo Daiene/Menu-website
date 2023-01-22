2804dc4b5dd595d08af1327f8ae561a12e60b073
Daiene Batagioti <daiene.montuani@hotmail.com>
2023-01-21 21:11:28 -0300

    Carousel working

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,34 +23,118 @@ listaSelecaoBotoes.forEach((botao) => {
     })
 })
 
+
 const controls = document.querySelectorAll(".control");
+const xSalada = document.querySelectorAll("#cartao-x-salada");
+const xBacon = document.querySelectorAll("#cartao-x-bacon");
+const xFrango = document.querySelectorAll("#cartao-x-frango");
+const xTudo = document.querySelectorAll("#cartao-x-tudo");
+const maxXSalada = xSalada.length;
+const maxBacon = xBacon.length;
+const maxFrango = xFrango.length;
+const maxTudo = xTudo.length;
 let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
 
 controls.forEach((control) => {
   control.addEventListener("click", () => {
     const isLeft = control.classList.contains("arrow-left");
 
     if (isLeft) {
-        currentItem -= 1;
-      } else {
-        currentItem += 1;
-      }
-  
-      if (currentItem >= maxItems) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    const idDoCardPrincipal = document.querySelector(".current-item");
+    const idValueDoCardASerExibido = idDoCardPrincipal.attributes.id.value;
+
+    if (idValueDoCardASerExibido === "cartao-x-salada") {
+      //Para o carrossel do x-salada
+      if (currentItem >= maxXSalada) {
         currentItem = 0;
       }
-  
+    
       if (currentItem < 0) {
-        currentItem = maxItems -1;
-      }
+        currentItem = 1;
+      }   
+    }
 
-    items.forEach((item) => {
-        items[currentItem].scrollIntoView({
-            behavior: "smooth",
-            inline: "center"
-          });
+    if (idValueDoCardASerExibido === "cartao-x-bacon") {
+      //Para o carrossel do x-bacon
+      if (currentItem >= maxBacon) {
+        currentItem = 0;
+      }
+    
+      if (currentItem < 0) {
+        currentItem = 1;
+      }   
+    }
+
+    if (idValueDoCardASerExibido === "cartao-x-frango") {
+      //Para o carrossel do x-frango
+      if (currentItem >= maxFrango) {
+        currentItem = 0;
+      }
+    
+      if (currentItem < 0) {
+        currentItem = 1;
+      }   
+    }
+
+    if (idValueDoCardASerExibido === "cartao-x-tudo") {
+      //Para o carrossel do x-tudo
+      if (currentItem >= maxTudo) {
+        currentItem = 0;
+      }
+    
+      if (currentItem < 0) {
+        currentItem = 1;
+      }   
+    }
+
+
+    //Para rolar entre um item e outro do carrossel do x-salada
+    xSalada.forEach((item) => item.classList.remove("current-item"));
+
+    xSalada[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
     });
+
+    xSalada[currentItem].classList.add("current-item");
+
+    
+    //Para rolar entre um item e outro do carrossel do x-bacon
+    xBacon.forEach((item) => item.classList.remove("current-item"));
+
+    xBacon[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    xBacon[currentItem].classList.add("current-item");
+
+
+    //Para rolar entre um item e outro do carrossel do x-frango
+    xFrango.forEach((item) => item.classList.remove("current-item"));
+
+    xFrango[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    xFrango[currentItem].classList.add("current-item");
+
+
+    //Para rolar entre um item e outro do carrossel do x-tudo
+    xTudo.forEach((item) => item.classList.remove("current-item"));
+
+    xTudo[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    xTudo[currentItem].classList.add("current-item");
+
   });
 });
